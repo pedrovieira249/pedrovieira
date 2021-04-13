cde0987fd22bd03013bf2f2aa04e7b76ca475fd8
Pedro Vieira <angus.pedro@hotmail.com>
2021-04-13 18:47:45 -0300

    Enviando commit para o GitHub

---
+++ teste.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+    <head lang="pt-br">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Teste GIT</title>
+    </head>
+    <body>
+        <header class="main-header"><?php echo "Olá, Mundo"; ?></header>   
+        <main class="main-content">Conteúdo Principal do site</main> 
+        <footer class="main-footer"></footer>
+    </body>
+</html>
